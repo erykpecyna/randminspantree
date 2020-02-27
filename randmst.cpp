c1d2400483cdb811ca2d8cd7705e5ad762042916
Eryk Pecyna <eryk.pecyna@gmail.com>
2020-02-27 01:19:06 -0500

    double done

--- randmst.cpp
+++ randmst.cpp
@@ -3,7 +3,6 @@
 #include "UnionFind.h"
 
 int main(int argc, char* argv[]) {
-    // n = 128; 256; 512; 1024; 2048; 4096; 8192; 16384; 32768; 65536; 131072; 262144;
     int n = std::stoi(argv[2]);
     int num_trials = std::stoi(argv[3]);
     int d = std::stoi(argv[4]);
